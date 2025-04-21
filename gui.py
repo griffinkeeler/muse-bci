23d50a6d452bc49753883b6b2b573ba7b051e82b
Griffin <griffinbkeeler@gmail.com>
2025-04-21 18:17:42 -0500

    Updated GUI aesthetics

--- gui.py
+++ gui.py
@@ -8,8 +8,18 @@ from filter_raw_data import filter_eeg_csv
 from epoch_mne import get_blink_threshold
 
 def countdown(window):
-    """Background thread that outputs
-      "3...2...1" one character at a time."""
+    """
+    Background Thread
+    Outputs:
+      "3...2...1" one character at a time.
+    Events:
+        -THREE-: Outputs 3
+        -TWO-:   Outputs 2
+        -One-:   Outputs 1
+
+        -PERIOD[n]-: Outputs '.'
+        -OPEN_WINDOW_TWO: Opens the second window
+    """
 
     # Keys for each phase.
     phase_keys = {0: '-THREE-', 1: '-TWO-', 2: '-ONE-'}
@@ -34,53 +44,101 @@ def countdown(window):
                 sleep(0.25)
                 window.write_event_value(('-THREAD-', key), step)
 
-def blink_dot(windows):
-    """Background thread that changes the color of a red dot to
-    green every 5 seconds over 75 seconds."""
+def change_dot_color(windows):
+    """
+    Background thread that changes the color of a red dot to
+    green every 5 seconds over 75 seconds.
+
+    Events:
+        -RED_DOT-: Changes the dot color to red.
+        -GREEN_DOT-: Changes the dot color to green for 0.3 seconds.
+        -OPEN_WINDOW_THREE-: Saves the timestamps for each time the dot
+        turns green and opens the third window.
+    """
 
     # Background thread.
-    for i in range(0, 16):
+    for i in range(0, 5):
         # Closes the thread if window is closed.
         if i == 0:
             windows.write_event_value(('-THREAD-', '-RED_DOT-'), i)
         else:
             windows.write_event_value(('-THREAD-', '-GREEN_DOT-'), i)
             sleep(0.3)
-        if i == 15:
-            windows.write_event_value(('-THREAD-', '-RED_DOT-'), c)
-            sleep(3)
-            windows.write_event_value(('-THREAD-', '-DOT_DONE-'), i)
         for c in range(0, 5):
             windows.write_event_value(('-THREAD-', '-RED_DOT-'), c)
             sleep(1)
+        if i == 4:
+            windows.write_event_value(('-THREAD-', '-RED_DOT-'), c)
+            sleep(3)
+            windows.write_event_value(('-THREAD-', '-OPEN_WINDOW_THREE-'), i)
 
 def window_one():
     """The first window for the GUI."""
 
-    layout_one = [[sg.Push(),
-                   sg.Text("Welcome!", text_color='white',
-                           auto_size_text=True),
-                   sg.Push()],
-                  [sg.Push(),
+    layout_one = [[sg.Push(background_color='light grey'),
+                   sg.Text("Welcome!",
+                           text_color='black',
+                           auto_size_text=True,
+                           font=('Arial', 20),
+                           background_color='light grey'),
+                   sg.Push(background_color='light grey')],
+                  [sg.Push(background_color='light grey'),
                    sg.Text('Press "Begin" to start calibration.',
-                           text_color='gold'),
-                   sg.Push()],
-                  [sg.Push(), sg.Button('Begin'), sg.Push()],
-                  [sg.Push(), sg.Text(key='-INTRO-'), sg.Push()],
-                  [(sg.Text(key='-DIGIT_ONE-', text_color='white'),
-                    sg.Text(key='-P1-', text_color='white'),
-                    sg.Text(key='-P2-', text_color='white'),
-                    sg.Text(key='-P3-', text_color='white'),
-                    sg.Text(key='-DIGIT_TWO-', text_color='white'),
-                    sg.Text(key='-P4-', text_color='white'),
-                    sg.Text(key='-P5-', text_color='white'),
-                    sg.Text(key='-P6-', text_color='white'),
-                    sg.Text(key='-DIGIT_THREE-', text_color='white'))],
+                           text_color='black',
+                           font=('Times New Roman', 16),
+                           background_color='light grey'),
+                   sg.Push(background_color='light grey')],
+                  [sg.Text(" ", background_color='light grey')],
+                  [sg.Push(background_color='light grey'),
+                   sg.Button('Begin',
+                             font=('Helvetica', 16)),
+                   sg.Push(background_color='light grey')],
+                  [sg.Push(background_color='light grey'),
+                   sg.Text(key='-INTRO-',
+                           font=('Times New Roman', 16),
+                           text_color='black',
+                           background_color='light grey'),
+                   sg.Push(background_color='light grey')],
+                  [(sg.Push(background_color='light grey'),
+                    sg.Text(key='-DIGIT_ONE-',
+                            text_color='black',
+                            font=('Times New Roman', 14),
+                            background_color='light grey'),
+                    sg.Text(key='-P1-',
+                            text_color='black',
+                            background_color='light grey'),
+                    sg.Text(key='-P2-',
+                            text_color='black',
+                            background_color='light grey'),
+                    sg.Text(key='-P3-',
+                            text_color='black',
+                            background_color='light grey'),
+                    sg.Text(key='-DIGIT_TWO-',
+                            text_color='black',
+                            font=('Times New Roman', 14),
+                            background_color='light grey'),
+                    sg.Text(key='-P4-',
+                            text_color='black',
+                            background_color='light grey'),
+                    sg.Text(key='-P5-',
+                            text_color='black',
+                            background_color='light grey'),
+                    sg.Text(key='-P6-',
+                            text_color='black',
+                            background_color='light grey'),
+                    sg.Text(key='-DIGIT_THREE-',
+                            text_color='black',
+                            font=('Times New Roman', 14),
+                            background_color='light grey'),
+                    sg.Push(background_color='light grey'))]
                   ]
 
     # Window is created.
     window = sg.Window("Blink Calibrator", layout_one,
-                       size=(500, 300), auto_size_text=True, finalize=True)
+                       size=(400, 300),
+                       auto_size_text=True,
+                       background_color='light grey',
+                       finalize=True,)
 
     # Main Loop
     while True:
@@ -123,21 +181,34 @@ def window_one():
 def window_two():
     """The second window for the GUI."""
 
-    layout_two = [[sg.Push(background_color='white'),
-                   sg.Text('Focus on the red dot in front of you.\n'
-                            'Blink once each time the color changes.',
-                             text_color='black',
-                             background_color='white'),
-                   sg.Push(background_color='white')],
-                  [sg.Text(background_color='white')],
-                  [sg.Text(background_color='white')],
-                  [sg.Push(background_color='white'),
-                   sg.Text('🔴', key='-DOT-', background_color='white')
-                      , sg.Push(background_color='white')]
+    layout_two = [[sg.Push(background_color='light grey'),
+                   sg.Text('Sit completely still and focus on the red dot.',
+                           text_color='black',
+                           font=('Times New Roman', 20),
+                           auto_size_text=True,
+                           background_color='light grey')],
+                  [sg.Push(background_color='light grey'),
+                   sg.Text('Blink once each time the color changes.',
+                           text_color='black',
+                           font=('Times New Roman', 20),
+                           auto_size_text=True,
+                           background_color='light grey'),
+                   sg.Push(background_color='light grey')],
+                  [sg.Text(background_color='light grey')],
+                  [sg.Text(background_color='light grey')],
+                  [sg.Push(background_color='light grey'),
+                   sg.Text('🔴',
+                           key='-DOT-',
+                           font=('Arial,', 40),
+                           background_color='light grey'),
+                   sg.Push(background_color='light grey')]
                   ]
 
-    window = sg.Window("Calibration", layout_two,
-                           size=(250, 150), background_color='white', finalize=True)
+    window = sg.Window("Calibration",
+                       layout_two,
+                       size=(400, 300),
+                       background_color='light grey',
+                       finalize=True)
 
     # The time the stream starts
     stream_start_time = time.time()
@@ -145,7 +216,7 @@ def window_two():
     threading.Thread(target=raw_eeg_to_csv).start()
 
     # Begins the thread right away.
-    window.start_thread(lambda: blink_dot(window),
+    window.start_thread(lambda: change_dot_color(window),
                             ('-THREAD-', '-THREAD ENDED-'))
 
     # A list of times the dot turns green
@@ -166,7 +237,7 @@ def window_two():
                 green_dot_times.append(cue_time)
             elif event[1] == '-RED_DOT-':
                 window['-DOT-'].update('🔴')
-            elif event[1] == '-DOT_DONE-':
+            elif event[1] == '-OPEN_WINDOW_THREE-':
                 # Saves the corresponding blink timestamps to a csv
                 pd.DataFrame({'blink_cue_time': green_dot_times}
                              ).to_csv('blink_cue_timestamps.csv', index=False)
@@ -177,15 +248,34 @@ def window_two():
 def window_three():
     """The third window for the GUI."""
 
-    layout_three = [[sg.Text()], [sg.Text()], [sg.Text()],
-                    [sg.Text()],
-                    [sg.Text("Calibration Complete!")],
-                    [[sg.Push(), sg.Button('View Results'), sg.Push()]],
-                    [[sg.Push(), sg.Text(key='-VIEW_THRESHOLD-'), sg.Push()]],
-                    [sg.Text()], [sg.Text()], [sg.Text()]
+    layout_three = [[sg.Text(background_color='light grey')],
+                    [sg.Text(background_color='light grey')],
+                    [sg.Text(background_color='light grey')],
+                    [sg.Text(background_color='light grey')],
+                    [sg.Push(background_color='light grey'),
+                     sg.Text("Calibration Complete!",
+                             font=('Times New Roman', 20),
+                             text_color='black',
+                             background_color='light grey'),
+                     sg.Push(background_color='light grey')],
+                    [[sg.Push(background_color='light grey'),
+                      sg.Button('View Results',
+                                font=('Helvetica', 16)),
+                      sg.Push(background_color='light grey')]],
+                    [[sg.Push(background_color='light grey'),
+                      sg.Text(key='-VIEW_THRESHOLD-',
+                              background_color='light grey'),
+                      sg.Push(background_color='light grey')]],
+                    [sg.Text(background_color='light grey')],
+                    [sg.Text(background_color='light grey')],
+                    [sg.Text(background_color='light grey')]
                     ]
 
-    window = sg.Window("Calibration Complete", layout_three, finalize=True)
+    window = sg.Window("Calibration Complete",
+                       layout_three,
+                       finalize=True,
+                       size=(400, 300),
+                       background_color='light grey')
 
     threading.Thread(target=lambda: filter_eeg_csv(
         input_path='/Users/griffinkeeler/PycharmProjects/muse-bci/raw_eeg_data.csv',
